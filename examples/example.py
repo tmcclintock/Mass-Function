f252bd34131a4062730d2cde635ab408616bf5cf
Tom McClintock <tmcclintock@email.arizona.edu>
2017-02-13 16:33:13 -0800

    Added the other tinker parameters to the derivative. Need to update the emulator code as well.

--- examples/example.py
+++ examples/example.py
@@ -6,15 +6,15 @@ cosmo_dict = {"om":0.3,"ob":0.05,"ol":1.-0.3,\
                   "ns":3.0,"w0":.96,"wa":0.0}
 redshift = 0.0
 
-MF = TMF.MF_model(cosmo_dict,redshift)
+example_TMF = TMF.TMF_model(cosmo_dict,redshift)
 
 d,e,f,g = 1.97,1.0,0.51,1.228 #d,e,f,g
-MF.set_parameters(d,e,f,g)
+example_TMF.set_parameters(d,e,f,g)
 
-lM_bins = MF.make_bins(Nbins=20,lM_low=12,lM_high=15)
+lM_bins = example_TMF.make_bins(Nbins=20,lM_low=12,lM_high=15)
 Masses = np.mean(10**lM_bins,1) #The mid points of the bins, just for plotting
-n_z0 = MF.n_in_bins(lM_bins)
-n_z1 = MF.n_in_bins(lM_bins,1.0)
+n_z0 = example_TMF.n_in_bins(lM_bins)
+n_z1 = example_TMF.n_in_bins(lM_bins,1.0)
 
 import matplotlib.pyplot as plt
 plt.loglog(Masses,n_z0,label=r"$z=0$")
--- tinker_mass_function.py
+++ tinker_mass_function.py
@@ -10,7 +10,7 @@ import numpy as np
 
 TOL = 1e-3
 
-class MF_model(object):
+class TMF_model(object):
     """
     This function takes in low and high log_10 mass
     values and creates bins (i.e. an Nbinsx2 array 
@@ -28,31 +28,27 @@ class MF_model(object):
     Optionally you can pass in log_10 of the mass bounds
     to build the spline between.
     """
-    def __init__(self,cosmo_dict,redshift,l10M_bounds=[11,16],use_numerical_derivatives=False):
+    def __init__(self,cosmo_dict,redshift,l10M_bounds=[11,16]):
         self.l10M_bounds = l10M_bounds #log_10 Mass bounds in Msun/h
         self.redshift = redshift
         self.scale_factor = 1./(1.+self.redshift)
         self.set_new_cosmology(cosmo_dict)
         self.params_are_set = False
-        self.use_numerical_derivatives = use_numerical_derivatives
 
     def set_parameters(self,d,e,f,g):
         """
-        Specify the tinker parameters and calculate quantities
-        that only depend on them.
+        Specify the tinker parameters and calculate
+        quantities that only depend on them.
         """
-
-        self.d = d
-        self.e = e
-        self.f = f
-        self.g = g
-        self.params = np.array([self.d,self.e,self.f,self.g])
+        self.params = np.array([d,e,f,g])
         self.B_coefficient = 2.0/(e**d * g**(-d/2.)*special.gamma(d/2.) + g**(-f/2.)*special.gamma(f/2.))
-        self.dBdd = self.B_coefficient**2/4.*e**d*g**(-d/2.)*special.gamma(d/2.)*(np.log(g)-2-special.digamma(d/2.))
-        self.dBde = self.B_coefficient**2/2.*(-d)*e**(d-1)*g**(-d/2.)*special.gamma(d/2)
-        self.dBdf = self.B_coefficient**2/4.*g**(-f/2.)*special.gamma(f/2.)*(np.log(g)-special.digamma(f/2.))
-        self.dBdg = self.B_coefficient**2/4.*(d*e**d*g**(-d/2.-1)*special.gamma(d/2.)+f*g**(-f/2.-1)*special.gamma(f/2.))
+        B = self.B_coefficient
+        self.dBdd = B**2/4.*e**d*g**(-d/2.)*special.gamma(d/2.)*(np.log(g)-2-special.digamma(d/2.))
+        self.dBde = B**2/2.*(-d)*e**(d-1)*g**(-d/2.)*special.gamma(d/2)
+        self.dBdf = B**2/4.*g**(-f/2.)*special.gamma(f/2.)*(np.log(g)-special.digamma(f/2.))
+        self.dBdg = B**2/4.*(d*e**d*g**(-d/2.-1)*special.gamma(d/2.)+f*g**(-f/2.-1)*special.gamma(f/2.))
         self.params_are_set = True
+        return
 
     def set_new_cosmology(self,cosmo_dict):
         G = 4.52e-48 #Newton's gravitional constant in Mpc^3/s^2/Solar Mass
@@ -62,6 +58,7 @@ class MF_model(object):
         self.rhom=Om*3.*(H0*Mpcperkm)**2/(8*np.pi*G*(H0/100.)**2)#Msunh^2/Mpc^3
         self.cosmo_dict = cosmo_dict
         self.build_splines()
+        return
 
     def build_splines(self):
         lM_min,lM_max = self.l10M_bounds
@@ -71,6 +68,7 @@ class MF_model(object):
         deriv_spline = ln_sig_inv_spline.derivative()
         self.deriv_spline = deriv_spline
         self.splines_built = True
+        return
 
     """
     The following functions are only used in 
@@ -117,56 +115,42 @@ class MF_model(object):
         return g_sigma * self.rhom * self.deriv_spline(M)
 
     """
-    This is the analytic derivative of n 
-    for each bin with respect to f and g.
+    This gives the uncertainty in the calculated quantity in each bin.
     """
-    def derivs_in_bins(self,lM_bins):
+    def derivs_in_bins(self,lM_bins,use_numerical_derivatives=False):
         lM_bins = np.log(10**lM_bins)
-        dndf,dndg = [],[]
-        for lMlow,lMhigh in lM_bins:
-            if not self.use_numerical_derivatives:
-                dndf.append(integrate.quad(self.ddf_dndlM_at_lM,lMlow,lMhigh,args=(self.params))[0])
-                dndg.append(integrate.quad(self.ddg_dndlM_at_lM,lMlow,lMhigh,args=(self.params))[0])
-            else:
-                params = np.copy(self.params)
-                Df = 0.1
-                Dg = 0.1
-                self.set_parameters(params[0],params[1],params[2]+Df/2.,params[3])
-                upper = integrate.quad(self.dndlM_at_lM,lMlow,lMhigh,args=(self.params))[0]
-                self.set_parameters(params[0],params[1],params[2]-Df/2.,params[3])
-                lower = integrate.quad(self.dndlM_at_lM,lMlow,lMhigh,args=(self.params))[0]
-                dndf.append((upper-lower)/Df)
-                self.set_parameters(params[0],params[1],params[2],params[3]+Dg/2.)
-                upper = integrate.quad(self.dndlM_at_lM,lMlow,lMhigh,args=(self.params))[0]
-                self.set_parameters(params[0],params[1],params[2],params[3]-Dg/2.)
-                lower = integrate.quad(self.dndlM_at_lM,lMlow,lMhigh,args=(self.params))[0]
-                dndg.append((upper-lower)/Dg)
-                self.set_parameters(params[0],params[1],params[2],params[3])
-        #Still need to propagate the derivatives here.
-        return np.array([dndf,dndg])
-
-    """
-    This is the variance in all bins.
-    The variable 'variances' contains the
-    variance in the paramters, namely f and g.
-    """
-    def variance_in_bins(self,lM_bins,variances):
-        dndf,dndg = self.derivs_in_bins(lM_bins)
-        return dndf**2*variances[0] + dndg**2*variances[1]
+        dndp = np.zeros((4,len(lM_bins)))
+        deriv_functions = [self.ddd_dndlM_at_lM,self.dde_dndlM_at_lM,self.ddf_dndlM_at_lM,self.ddg_dndlM_at_lM]
+        for i in range(len(lM_bins)):
+            lMlow,lMhigh = lM_bins[i]
+            for j in range(4):
+                if not use_numerical_derivatives: 
+                    dndp[j,i]= integrate.quad(deriv_functions[j],lMlow,lMhigh,args=(self.params))[0]
+                else:
+                    pc_up   = np.copy(self.params)
+                    pc_down = np.copy(self.params)
+                    Dp = 0.1
+                    pc_up[j]   += Dp/2.
+                    pc_down[j] -= Dp/2.
+                    self.set_parameters(pc_up[0],pc_up[1],pc_up[2],pc_up[3])
+                    upper = integrate.quad(self.dndlM_at_lM,lMlow,lMhigh,args=(self.params))[0]
+                    self.set_parameters(pc_down[0],pc_down[1],pc_down[2],pc_down[3])
+                    lower = integrate.quad(self.dndlM_at_lM,lMlow,lMhigh,args=(self.params))[0]
+                    dndp[j,i] = (upper-lower)/dp
+        return dndp
 
     """
     Similar to the above, this gives the full
     covariance matrix between the bins.
-    'Variances' contain the variance of f
-    and then the variance of g.
+    Cov_p contains the covariance matrix between d,e,f,g.
     """
-    def covariance_in_bins(self,lM_bins,variances,fg_covariance=0):
-        #Still need to propagate derivatives here.
-        dndf,dndg = self.derivs_in_bins(lM_bins)
+    def covariance_in_bins(self,lM_bins,Cov_p):
+        dndp = self.derivs_in_bins(lM_bins)
+        if len(np.shape(Cov_p)) == 1: Cov_p = np.diag(Cov_p)
         cov = np.zeros((len(lM_bins),len(lM_bins)))
         for i in range(len(lM_bins)):
-            cov[i] = dndf[i]*dndf*variances[0] + dndg[i]*dndg*variances[1] + fg_covariance*(dndf[i]*dndg + dndg[i]*dndf)
-            continue #end i
+            for j in range(len(lM_bins)):
+                cov[i,j] = np.dot(dndp[:,i],np.dot(Cov_p,dndp[:,j]))
         return cov
 
     """
@@ -180,19 +164,8 @@ class MF_model(object):
                 self.redshift = redshift
                 self.scale_factor = 1./(1.+redshift)
                 self.build_splines()
-        lM_bins = np.log(10**lM_bins)
+        lM_bins = np.log(10**lM_bins) #switch to natural log
         return np.array([integrate.quad(self.dndlM_at_lM,lMlow,lMhigh,args=(self.params),epsabs=TOL,epsrel=TOL/10.)[0] for lMlow,lMhigh in lM_bins])
-
-    def n_approx_in_bins(self,lM_bins,redshift=None):
-        if not self.params_are_set: raise ValueError("Must set parameters before modeling.")
-        if redshift is not None:
-            if redshift != self.redshift:
-                self.redshift = redshift
-                self.scale_factor = 1./(1.+redshift)
-                self.build_splines()
-        lM_bins = np.log(10**lM_bins)
-        answer = np.array([0.5*(lM_high-lM_low)*(self.dndlM_at_lM(lM_high,self.params)+self.dndlM_at_lM(lM_low,self.params)) for lM_low,lM_high in lM_bins])
-        return answer
 
 if __name__ == "__main__":
     #An example cosmology
@@ -200,23 +173,32 @@ if __name__ == "__main__":
                   "ok":0.0,"h":0.7,"s8":0.77,\
                   "ns":3.0,"w0":.96,"wa":0.0}
 
-    MF = MF_model(cosmo_dict,redshift=0.0)
+    #Create a TMF object
+    TMF = TMF_model(cosmo_dict,redshift=0.0)
 
-    d,e,f,g = 1.97,1.0,0.51,1.228 #d,e,f,g
-    MF.set_parameters(d,e,f,g)
+    #The tinker parameters -- example values
+    TMF.set_parameters(1.97,1.0,0.51,1.228)
 
-    lM_bins = MF.make_bins(Nbins=10,lM_low=12,lM_high=15)
+    #Some pretend bins in log_10 M_sun/h
+    lM_bins = TMF.make_bins(Nbins=10,lM_low=13,lM_high=15)
     Masses = np.mean(10**lM_bins,1) #The mid points of the bins, just for plotting
-    n_z0 = MF.n_in_bins(lM_bins)
-    n_var_z0 = MF.variance_in_bins(lM_bins,[0.01,0.01])
-    n_z1 = MF.n_in_bins(lM_bins,1.0)
+    n_z0 = TMF.n_in_bins(lM_bins)
+    Cov_p = np.diag(np.ones((4))*0.01)
+    n_cov_z0 =TMF.covariance_in_bins(lM_bins,Cov_p)
+    n_err_z0 = np.sqrt(np.diagonal(n_cov_z0))
 
-    print MF.derivs_in_bins(lM_bins)
+    #Now at z=1.0
+    n_z1 = TMF.n_in_bins(lM_bins,redshift=1.0)
+    n_cov_z1 =TMF.covariance_in_bins(lM_bins,Cov_p)
+    n_err_z1 = np.sqrt(np.diagonal(n_cov_z1))
 
     import matplotlib.pyplot as plt
-    plt.loglog(Masses,n_z1,label=r"$z=1$")
-    plt.errorbar(Masses,n_z0,np.sqrt(n_var_z0),label=r"$z=0$")
-    plt.ylabel(r"$n\ [h^3{\rm Mpc^{-3}}]$")
-    plt.xlabel(r"$M\ [h^{-1}{\rm M}_\odot]$")
+    plt.errorbar(Masses,n_z0,n_err_z0,label=r"$z=0$")
+    plt.errorbar(Masses,n_z1,n_err_z1,label=r"$z=1$")
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel(r"$M\ [h^{-1}{\rm M}_\odot]$",fontsize=24)
+    plt.ylabel(r"$n\ [h^3{\rm Mpc^{-3}}]$",fontsize=24)
+    plt.subplots_adjust(bottom=0.15)
     plt.legend()
     plt.show()
